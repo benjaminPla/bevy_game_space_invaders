333b72cf319f7f4ccc99becfa44d90b4b17cfac7
ben <benjamin.pla@kayakomat.com>
2025-02-14 10:00:56 +0100

    update player movement

--- src/player.rs
+++ src/player.rs
@@ -8,25 +8,19 @@ pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup_player);
-        app.add_systems(Update, (player_input, player_movement));
+        app.add_systems(Update, player_movement);
     }
 }
 
 #[derive(Component)]
 pub struct Player {
-    position: Vec2,
     speed: f32,
-    pub direction: Vec2,
 }
 
 impl Player {
     pub fn new() -> Self {
-        Self {
-            position: Vec2::ZERO,
-            speed: 200.0,
-            direction: Vec2::ZERO,
-        }
-    }
+        Self { speed: 200.0 }
+   }
 }
 
 fn setup_player(
@@ -51,43 +45,38 @@ fn setup_player(
             }),
             ..default()
         },
-        Transform::from_xyz(0., 0., 0.).with_scale(Vec3::splat(resolution.pixel_ratio)),
+        Transform::from_xyz(0., (resolution.screen_dimensions.y / 3.5) * -1., 0.)
+            .with_scale(Vec3::splat(resolution.pixel_ratio)),
         Player::new(),
         animation_config,
     ));
 }
 
-fn player_movement(time: Res<Time>, mut query: Query<(&mut Transform, &mut Player)>) {
-    for (mut transform, mut player) in query.iter_mut() {
-        let delta_move = player.direction * player.speed * time.delta_secs();
-        player.position += delta_move;
+fn player_movement(
+    keys: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+    mut query: Query<(&mut Transform, &mut Player)>,
+    resolution: Res<resolution::Resolution>,
+) {
+    for (mut transform, player) in query.iter_mut() {
+        let boundary_x = resolution.screen_dimensions.x / 2.7;
 
-        transform.translation.x = player.position.x;
-        transform.translation.y = player.position.y;
-    }
-}
+        let moving_left = keys.pressed(KeyCode::KeyA) || keys.pressed(KeyCode::ArrowLeft);
+        let moving_right = keys.pressed(KeyCode::KeyD) || keys.pressed(KeyCode::ArrowRight);
 
-fn player_input(keys: Res<ButtonInput<KeyCode>>, mut query: Query<&mut Player>) {
-    let mut movement = Vec2::ZERO;
+        let mut movement_direction = 0.0;
+        if moving_left {
+            movement_direction = -1.0;
+        }
+        if moving_right {
+            movement_direction = 1.0;
+        }
 
-    if keys.pressed(KeyCode::KeyW) {
-        movement.y += 1.0;
-    }
-    if keys.pressed(KeyCode::KeyS) {
-        movement.y -= 1.0;
-    }
-    if keys.pressed(KeyCode::KeyA) {
-        movement.x -= 1.0;
-    }
-    if keys.pressed(KeyCode::KeyD) {
-        movement.x += 1.0;
-    }
+        let delta_move = movement_direction * player.speed * time.delta_secs();
+        let new_position_x = transform.translation.x + delta_move;
 
-    if let Some(mut player) = query.iter_mut().next() {
-        player.direction = if movement.length() > 0.0 {
-            movement.normalize()
-        } else {
-            Vec2::ZERO
-        };
+        if new_position_x.abs() <= boundary_x {
+            transform.translation.x = new_position_x;
+        }
     }
 }
